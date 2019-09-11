282c6cf732c8191a41a58a5a26d2928db980e30d
DHANUSH <ds676@njit.edu>
2019-09-11 14:24:22 -0400

    changed styles and html and php

--- frontend/login.php
+++ frontend/login.php
@@ -1,31 +1,10 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Login</title>
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        <div class="single-card">
-            <div class="card">
-
-                <h1 class="card-title">Login</h1>
-
-                <form action="" method="post">
-                    <label>Username:</label>
-                    <input type="text" name="username">
-
-                    <label>Password:</label>
-                    <input type="password" name="password">
-
-                    <button type="submit">Login</button>
-					
-					<?php
+	<?php
 						if (!empty($_POST)) {
 							$data = array(
 							"username" => $_POST["username"],
 							"password" => $_POST["password"]
 							);
-							
+
 							$url = 'https://web.njit.edu/~ms2285/cs490/middle/middle.php';
 							$ch = curl_init($url);
 							$postData = http_build_query($data);
@@ -34,21 +13,16 @@
 							curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 							$response = json_decode(curl_exec($ch), true);
 							curl_close($ch);
-							
-							if($response["custom"]) {
-								echo '<p class="card-content">Custom logged in.</p>';
+
+							if($response["gradesystem"]) {
+								echo '<p class="card-content">Logged into Grading System.</p>';
 							} else {
-								echo '<p class="card-content">Custom failed to login.</p>';
+								echo '<p class="card-content">Failed to login to grading system</p>';
 							}
 							if($response["njit"]) {
-								echo '<p class="card-content">NJIT logged in.</p>';
+								echo '<p class="card-content">logged into NJIT.</p>';
 							} else {
-								echo '<p class="card-content">NJIT failed to login.</p>';
+								echo '<p class="card-content">failed to log into NJIT.</p>';
 							}
 						}
 					?>
-                </form>
-            </div>
-        </div>
-    </body>
-</html>
