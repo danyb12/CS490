4ce08ae6f1b8e1b58e0d8186293aecd7adf7fb3d
Beraval <msemren989@gmail.com>
2019-09-19 10:33:07 -0400

    Update middle.php

--- middle/middle.php
+++ middle/middle.php
@@ -1,19 +1,37 @@
 <?php
-header("Content-Type:application/json");
-$data = $_POST;
+//function for sendind curl calls
+function curl($url, $data) {
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$resp = curl_exec($ch);
+	curl_close($ch);
+	return $resp;
+}
 
-$ch = curl_init('https://web.njit.edu/~ms2285/cs490/backend/backend.php');
-$postData = http_build_query($data);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = json_decode(curl_exec($ch), true);
-curl_close($ch);
+//Custom curl data
+$dataCust = $_POST;
 
-$response = array(
-"custom" => $response["login"],
-"njit" => false
+//Njit curl data
+$dataNjit = array (
+"ucid" => $_POST["username"],
+"pass" => $_POST["password"]
 );
 
+//Send both curl calls
+$respCust = curl('https://web.njit.edu/~ms2285/cs490/backend/backend.php', $dataCust);
+$respNjit = curl('https://aevitepr2.njit.edu/MyHousing/login.cfm', $dataNjit);
+
+//Build response data
+$regex = "/Residence Life myHousing Login/";
+$response = array(
+"custom" => json_decode($respCust, true)["login"],
+"njit" => !preg_match($regex, $respNjit)
+);
+
+//Set resposne data type
+header("Content-Type:application/json");
+//Send response
 echo json_encode($response);
 ?>
